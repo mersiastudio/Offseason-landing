d291cc532e8f1d2f4c755a37be973358e7e51b67
Joey Gaffney <joeygaffney22@gmail.com>
2023-07-30 01:33:40 -0700

    first

---
+++ script.js
@@ -0,0 +1,16 @@
+// Wait for the DOM to be loaded
+document.addEventListener("DOMContentLoaded", function () {
+  // Words to be displayed one at a time
+  const words = ["Perfected", "Simplified", "Elevated"];
+
+  // Initialize Typed.js
+  const typed = new Typed(".type-text", {
+    strings: words,
+    typeSpeed: 80, // Speed of typing in milliseconds
+    backSpeed: 50, // Speed of deleting in milliseconds
+    loop: true, // Whether to loop through the words
+    loopCount: Infinity, // Number of loops (set to Infinity for infinite loop)
+    showCursor: false, // Whether to display the cursor
+    shuffle: false, // Whether to shuffle the words before displaying
+  });
+});
